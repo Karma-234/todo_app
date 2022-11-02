815f043a2a3b79eb0a5dbe1c8b6068c34a2c73dc
kamal Busari <busari1kamal@gmail.com>
2022-11-02 21:08:35 +0800

    added services

--- lib/services/todo_service.dart
+++ lib/services/todo_service.dart
@@ -1,3 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/database/todo_database.dart';
 
-class TodoService with ChangeNotifier {}
+import '../models/todo.dart';
+
+class TodoService with ChangeNotifier {
+  List _todos = [];
+
+  List get todos => _todos;
+  Future<String> getTodos(String username) async {
+    try {
+      _todos = await TodoDatabase.instance.getTodos(username);
+      notifyListeners();
+    } catch (e) {
+      return e.toString();
+    }
+    return 'OK';
+  }
+
+  Future<String> deleteTodos(Todo todo) async {
+    try {
+      await TodoDatabase.instance.deleteTodo(todo);
+    } catch (e) {
+      return e.toString();
+    }
+    String result = await getTodos(todo.username);
+    return result;
+  }
+
+  Future<String> createTodo(Todo todo) async {
+    try {
+      await TodoDatabase.instance.createTodo(todo);
+    } catch (e) {
+      return e.toString();
+    }
+    String result = await getTodos(todo.username);
+    return result;
+  }
+
+  Future<String> toggleTodoDone(Todo todo) async {
+    try {
+      await TodoDatabase.instance.toggleTodoDone(todo);
+    } catch (e) {
+      return e.toString();
+    }
+    String result = await getTodos(todo.username);
+    return result;
+  }
+}
--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,6 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/models/user.dart';
 
-class UserService with ChangeNotifier {}
+import '../database/todo_database.dart';
+
+class UserService with ChangeNotifier {
+  late User _currentUser;
+  bool _busyCreate = false;
+  bool _userexists = false;
+
+  User get currenUser => _currentUser;
+  bool get userExists => _userexists;
+  bool get busycreate => _busyCreate;
+
+  set userExists(bool value) {
+    _userexists = value;
+    notifyListeners();
+  }
+
+  Future<String> getUser(String username) async {
+    try {
+      _currentUser = await TodoDatabase.instance.getUser(username);
+      notifyListeners();
+    } catch (e) {
+      return e.toString();
+    }
+    return "OK";
+  }
+
+  Future<String> checkIfUserExists(String username) async {
+    try {
+      await TodoDatabase.instance.getUser(username);
+      notifyListeners();
+    } catch (e) {
+      return e.toString();
+    }
+    return "OK";
+  }
+
+  Future<String> updateCurrentUser(String name) async {
+    _currentUser.name = name;
+    notifyListeners();
+    try {
+      await TodoDatabase.instance.updateUser(_currentUser);
+    } catch (e) {
+      return e.toString();
+    }
+    return 'OK';
+  }
+
+  Future<String> createUser(User user) async {
+    _busyCreate = true;
+    notifyListeners();
+    try {
+      await TodoDatabase.instance.createUser(user);
+    } catch (e) {
+      return e.toString();
+    }
+    _busyCreate = false;
+    notifyListeners();
+    return 'OK';
+  }
+}
 
 String getHumanReadableError(String message) {
   if (message.contains('UNIQUE constraint failed')) {
